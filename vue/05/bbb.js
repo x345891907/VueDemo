fbede64d917842413ce1b1fed939c7d0af4b9a71
x345891907 <345891907@qq.com>
2020-01-07 10:19:37 +0800

    初始化文件

---
+++ vue/05/bbb.js
@@ -0,0 +1,5 @@
+var name = 'bbb---阿秋';
+var flag = false;
+if(flag){
+	console.log(sum(20,30));
+}
---
+++ vue/05/mmm.js
@@ -0,0 +1,6 @@
+import {flag,sum} from './aaa.js';
+
+if(flag){
+	console.log('mmm----阿秋是傻子, 哈哈哈');
+	console.log(sum(20,30));
+}
